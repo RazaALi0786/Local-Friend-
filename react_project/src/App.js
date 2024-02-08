81010b8a94a4b447138adac93dad1b16f44b1569
Syed Raza Ali <razaali26296@gmail.com>
2024-02-08 23:57:20 +0530

    second commit by ali

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,16 +1,23 @@
 import Login from "./pages/login/Login";
 
-import "./App.css"
+import "./App.css";
 import Signup from "./pages/signup/Signup";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-
-  return <div className="text-white h-screen flex justify-center items-center bg-cover" style={{"background": "url('https://images.pexels.com/photos/1496378/pexels-photo-1496378.jpeg?auto=compress&cs=tinysrgb&w=600"}}>
-    <Routes>
-      <Route path="login" element={<Login/>}/>
-    </Routes>
-  </div>;
+  return (
+    <div
+      className="flex items-center justify-center h-screen text-white bg-cover "
+      style={{
+        background:
+          "url('https://images.pexels.com/photos/1496378/pexels-photo-1496378.jpeg?auto=compress&cs=tinysrgb&w=600",
+      }}
+    >
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
