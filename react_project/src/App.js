24e0cc182f6211554fd717b1dfd47a16f304a5ab
Faisal_Khan <fk20253@gmail.com>
2024-02-08 20:53:38 +0530

    first commit from faisal

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="text-white bg-red-600">Hello tabrez chad</div>;
+  return (
+    <div className="text-white bg-red-600 bg-green-900">
+      Hello tabrez chad raza chusa laga lo
+    </div>
+  );
 }
 
 export default App;
