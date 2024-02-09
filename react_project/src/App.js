e808fee4b193bf9fc6462db926fec688e049f3e8
tabrez07866 <tabrezkhan100820033ss@gmail.com>
2024-02-09 15:24:16 +0530

    bg-img

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -11,7 +11,7 @@ function App() {
       className="flex items-center justify-center h-screen text-white bg-cover "
       style={{
         background:
-          "url('https://th.bing.com/th/id/R.581b26c3727c37ef44a08b382a59359e?rik=0jGMXSD8jj5vTw&riu=http%3a%2f%2fwallpapercave.com%2fwp%2fzwaIWUh.jpg&ehk=SdSfqPHy5aQwka7ox7nlPO5Zhbm8t1v7m6ICvlY64Cs%3d&risl=&pid=ImgRaw&r=0",
+          "url('https://wallup.net/wp-content/uploads/2018/09/27/13853-gradient.jpg"
       }}
     >
       <Routes>
