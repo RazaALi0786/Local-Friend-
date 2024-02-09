a352c372643ec47cb69c7d25ffd56bb0a2d34296
tabrez07866 <tabrezkhan100820033ss@gmail.com>
2024-02-09 16:13:24 +0530

    connected-firebase

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,11 +1,23 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/signup/Register";
-
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
+import { getDatabase,ref,set } from 'firebase/database';
+import {app} from './firebase'
+const db=getDatabase(app);
+
 function App() {
+
+  const pushData=()=>{
+    set(ref(db,'users/tabrez'),{
+      id:1,
+      name:'tabrez khan',
+      age:20,
+    })
+  }
+
   return (
     <div
       className="flex items-center justify-center h-screen text-white bg-cover "
@@ -14,6 +26,7 @@ function App() {
           "url('https://wallup.net/wp-content/uploads/2018/09/27/13853-gradient.jpg"
       }}
     >
+      <button onClick={pushData}> put data</button>
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="Register" element={<Register />} />
