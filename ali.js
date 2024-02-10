d6fa4d1838cdd545c475871d3bc28fac2d9cc461
Syed Raza Ali <razaali26296@gmail.com>
2024-02-10 17:36:48 +0530

    formatted

---
+++ ali.js
@@ -0,0 +1,12 @@
+const navbar = (
+  <nav>
+    <h1>Hello</h1>
+    <ul>
+      <li>Hi</li>
+      <li>This</li>
+      <li>Is</li>
+      <li>Ali</li>
+    </ul>
+  </nav>
+);
+ReactDOM.render(render, document.getElementById("root"));
--- react_project/src/index.js
+++ react_project/src/index.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Login from './pages/login/Login'
-import Register from './pages/signup/Register'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/signup/Register";
 
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"login",
-        element:<Login/>
+        path: "login",
+        element: <Login />,
       },
       {
-        path:"register",
-        element:<Register/>
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
-      }
-    ]
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-     <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
